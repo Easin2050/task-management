a90bd8830a3812c33d13af02f4e562f9206039b3
easin <easin562050@gmail.com>
2025-02-13 09:39:59 +0600

    This is the module-7 code and it maintain database sql code

--- tasks/views.py
+++ tasks/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from tasks.forms import TaskForm,TaskModelForm
-from tasks.models import Employee,Task
+from tasks.models import Employee,Task,TaskDetail,Project
+from datetime import date
+from django.db.models import Q,Count,Max,Min,Avg
 def manager_dashboard(request):
     return render(request,"dashboard/manager-dashboard.html")
 
@@ -41,11 +43,33 @@ def create_task(request):
      return render(request,"task_form.html",context)
 
 def view_task(request):
-     #Retrive all data from the task model
-     tasks=Task.objects.all()
-     #retrive a specific task
-     task_3=Task.objects.get(id=1)
-     #Fetch the first task
-     first_task=Task.objects.first()
-     return render(request,"show_task.html",{"tasks":tasks, "task3":task_3,'first_task':first_task})
+     # #Retrive all data from the task model
+     # tasks=Task.objects.all()
+     # #retrive a specific task
+     # task_3=Task.objects.get(id=1)
+     # #Fetch the first task
+     # first_task=Task.objects.first()
+     # tasks=Task.objects.filter(status="PENDING")
+     # tasks=Task.objects.filter(status="COMPLETED")
+     # return render(request,"show_task.html",{"tasks":tasks, "task3":task_3,'first_task':first_task})
+     # shwo the task which due day is today
+     # tasks=Task.objects.filter(due_date='date.today')
+     """Show the task whose proirity is low"""
+     # tasks=TaskDetail.objects.exclude(priority="L")
+     """Show the task that contain word paper and status pending"""
+     # tasks=Task.objects.filter(title__icontains="c",status="PENDING")
+     #Show the task which are pending or in-progress
+     # tasks=Task.objects.filter(Q(status="PENDING")| Q(status="IN_PROGRESS"))
+     # tasks=Task.objects.filter(status="kfjdf").exists()
+
+     #Sekect realated(Foreign key and One to One field )
+     # tasks=Task.objects.select_related('details'.all())
+     # tasks=TaskDetail.objects.select_related('task').all()
+     # tasks=Task.objects.select_related('project').all()
+     """Prefetch_related (reverse Foreignkey ,manay to many)"""
+     # tasks=Project.objects.prefetch_related("task_set").all()
+     # tasks=Project.objects.prefetch_related("assigned_to)").all()
+     # task_count=Task.objects.aggregate(num_task=Count('id'))
+     projects=Project.objects.annotate(num_task=Count('task')).order_by('num_task')
+     return render(request,"show_task.html",{"projects":projects})
 
