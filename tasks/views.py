6bf5ea1035e99ff8a0fbba41960e7948f2ec8d61
easin <easin562050@gmail.com>
2025-03-24 00:43:03 +0600

    Module-12 is done and working correct

--- tasks/views.py
+++ tasks/views.py
@@ -5,19 +5,16 @@ from tasks.models import Employee,Task,TaskDetail,Project
 from datetime import date
 from django.db.models import Q,Count,Max,Min,Avg
 from django.contrib import messages
+from django.contrib.auth.decorators import user_passes_test,login_required,permission_required
 
+def is_manager(user):
+    return user.groups.filter(name='Manager').exists()
+
+def is_employee(user):
+    return user.groups.filter(name='Manager').exists()
+
+@user_passes_test(is_manager, login_url='no-permission')
 def manager_dashboard(request):
-     # Getting task count
-     # total_task=tasks.count()
-     # completed_task=Task.objects.filter(status="COMPLETED").count()
-     # in_progress_task=Task.objects.filter(status="IN_PROGRESS").count()
-     # pending_task=Task.objects.filter(status="PENDING").count()
-     # count={
-     #      'total_task':,
-     #      'completed_task':,
-     #      'in_progress_task':,
-     #      'pending_task':
-     # }
      type=request.GET.get('type','all')
           
      counts=Task.objects.aggregate(
@@ -45,7 +42,7 @@ def manager_dashboard(request):
      return render(request,"dashboard/manager-dashboard.html",context)
 
 
-
+@user_passes_test(is_employee)
 def user_dashboard(request):
      return render(request, "dashboard/user-dashboard.html")
 
@@ -55,8 +52,9 @@ def test(request):
      }
      return render(request,"test.html",context)
 
+@login_required
+@permission_required("tasks.add_task", login_url='no-permission')
 def create_task(request):
-     # employees=Employee.objects.all()
      task_form=TaskModelForm()#employees=employees)# For GET
      task_detail_form=TaskDetailModelForm()
      if request.method=="POST":
@@ -70,56 +68,22 @@ def create_task(request):
                task_detail.save()
                messages.success(request,'Task created successfully')
                return redirect('create-task')
-               """For Django from Data"""
-               # data=form.cleaned_data
-               # title=data.get("title")
-               # description=data.get("description")
-               # due_date=data.get("due_date")
-               # assigned_to=data.get("assigned_to")
-
-               # task=Task.objects.create(title=title,description=description,due_date=due_date)
-               
-               # #Assign employee to tasks
-               # for emp_id in assigned_to:
-               #      employee=Employee.objects.get(id=emp_id)
-               #      task.assigned_to.add(employee)
+          
                
      context={"task_form":task_form,"task_detail_form":task_detail_form}
      return render(request,"task_form.html",context)
 
+@login_required
+@permission_required("tasks.view_task", login_url='no-permission')
 def view_task(request):
-     # #Retrive all data from the task model
-     # tasks=Task.objects.all()
-     # #retrive a specific task
-     # task_3=Task.objects.get(id=1)
-     # #Fetch the first task
-     # first_task=Task.objects.first()
-     # tasks=Task.objects.filter(status="PENDING")
-     # tasks=Task.objects.filter(status="COMPLETED")
-     # return render(request,"show_task.html",{"tasks":tasks, "task3":task_3,'first_task':first_task})
-     # shwo the task which due day is today
-     # tasks=Task.objects.filter(due_date='date.today')
-     """Show the task whose proirity is low"""
-     # tasks=TaskDetail.objects.exclude(priority="L")
-     """Show the task that contain word paper and status pending"""
-     # tasks=Task.objects.filter(title__icontains="c",status="PENDING")
-     #Show the task which are pending or in-progress
-     # tasks=Task.objects.filter(Q(status="PENDING")| Q(status="IN_PROGRESS"))
-     # tasks=Task.objects.filter(status="kfjdf").exists()
-
-     #Sekect realated(Foreign key and One to One field )
-     # tasks=Task.objects.select_related('details'.all())
-     # tasks=TaskDetail.objects.select_related('task').all()
-     # tasks=Task.objects.select_related('project').all()
-     """Prefetch_related (reverse Foreignkey ,manay to many)"""
-     # tasks=Project.objects.prefetch_related("task_set").all()
-     # tasks=Project.objects.prefetch_related("assigned_to)").all()
-     # task_count=Task.objects.aggregate(num_task=Count('id'))
+     
      projects=Project.objects.annotate(num_task=Count('task')).order_by('num_task')
      return render(request,"show_task.html",{"projects":projects})
 
+
+@login_required
+@permission_required("tasks.change_task", login_url='no-permission')
 def update_task(request,id):
-     # employees=Employee.objects.all()
      task=Task.objects.get(id=id)
      task_form=TaskModelForm(instance=task)#employees=employees)# For GET
      if task.details:
@@ -136,22 +100,12 @@ def update_task(request,id):
                messages.success(request,'Task updated successfully')
                return redirect('update-task', id)
                """For Django from Data"""
-               # data=form.cleaned_data
-               # title=data.get("title")
-               # description=data.get("description")
-               # due_date=data.get("due_date")
-               # assigned_to=data.get("assigned_to")
-
-               # task=Task.objects.create(title=title,description=description,due_date=due_date)
-               
-               # #Assign employee to tasks
-               # for emp_id in assigned_to:
-               #      employee=Employee.objects.get(id=emp_id)
-               #      task.assigned_to.add(employee)
-               
+              
      context={"task_form":task_form,"task_detail_form":task_detail_form}
      return render(request,"task_form.html",context)
 
+@login_required
+@permission_required("tasks.delete_task", login_url='no-permission')
 def delete_task(request,id):
      if request.method=="POST":
           task=Task.objects.get(id=id)
