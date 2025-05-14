67a9d204aa5bf363b5e2eabcb8cb56f6b7790bf8
easin <easin562050@gmail.com>
2025-05-14 22:49:26 +0600

    custom filtter updated

--- users/templatetags/custom_filtters.py
+++ users/templatetags/custom_filtters.py
@@ -2,17 +2,18 @@ from django import template
 from datetime import datetime
 from django.utils import timezone
 
-register=template.Library() # This is for register the library and rigister will be act like a object.
+register = template.Library()
 
-@register.filter # By calling this object i can use it like a decoretor
+
+@register.filter
 def humanized_date(value):
     if value:
-        today=datetime.now().date()
-        value=timezone.localtime(value)
-        if value.date()==today:
+        today = datetime.now().date()
+        value = timezone.localtime(value)
+        if value.date() == today:
             return f"Today at {value.strftime('%I:%M %p')}"
-        elif value.date()==today.replace(day=today.day-1):
-            return f"Yesterday at {value.strftime('%I %M %p')}"
+        if value.date() == today.replace(day=today.day - 1):
+            return f"Yesterday at {value.strftime('%I:%M %p')}"
         else:
-            return f"{value.date().strftime('%B %d')},value.strftime('%I %M %p')"
-    return 'No login record available'
+            return f"{value.date().strftime('%B %d')}, {value.strftime('%I:%M %p')}"
+    return "No login record available"
